a3d67318e5fbdaf8025befdce0c4f1c168849e23
studboo <49799191+studboo@users.noreply.github.com>
2021-07-10 03:17:21 +0530

    Create ans.js

---
+++ ans.js
@@ -0,0 +1,40 @@
+const data = {
+  cuisine_name_ar: 'AR cuisine_name_ar',
+  cuisine_name_fr: 'FR cuisine_name_fr',
+  main_category_name_ar: 'AR category_name_ar',        
+  main_category_name_fr: 'FR category_name_fr',        
+  data: {
+    cuisine_name_ar: 'AR cuisine_name_ar',
+    cuisine_name_fr: 'FR cuisine_name_fr',
+    main_category_name_ar: 'AR category_name_ar',      
+    main_category_name_fr: 'FR category_name_fr',      
+    data: {
+      cuisine_name_ar: 'AR cuisine_name_ar',
+      cuisine_name_fr: 'FR cuisine_name_fr',
+      main_category_name_ar: 'AR category_name_ar',    
+      main_category_name_fr: 'FR category_name_fr',    
+      data: {
+        cuisine_name_ar: 'AR cuisine_name_ar',
+        cuisine_name_fr: 'FR cuisine_name_fr',
+        main_category_name_ar: 'AR category_name_ar',  
+        main_category_name_fr: 'FR category_name_fr',  
+        data: {
+          cuisine_name_ar: 'AR cuisine_name_ar',       
+          cuisine_name_fr: 'FR cuisine_name_fr',       
+          main_category_name_ar: 'AR category_name_ar',
+          main_category_name_fr: 'FR category_name_fr',
+          cuisine_name: 'EN cuisine_name_en',
+          main_category_name: 'EN category_name_en'    
+        },
+        cuisine_name: 'EN cuisine_name_en',
+        main_category_name: 'EN category_name_en'      
+      },
+      cuisine_name: 'EN cuisine_name_en',
+      main_category_name: 'EN category_name_en'        
+    },
+    cuisine_name: 'EN cuisine_name_en',
+    main_category_name: 'EN category_name_en'
+  },
+  cuisine_name: 'EN cuisine_name_en',
+  main_category_name: 'EN category_name_en'
+}
